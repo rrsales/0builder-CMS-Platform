863242af67fec570a65c43557356a925b215cbf8
rrsales <josephbskinner1@gmail.com>
2025-12-03 19:35:53 -0500

    Update pages.js

--- modules/pages.js
+++ modules/pages.js
@@ -12,22 +12,18 @@ export function setPages(data) {
 function renderPages() {
   pagesPanel.innerHTML = `
     <div class="panelHeader">
-      <h2>Pages</h2>
-      <button id="addPageBtn"><i class="fa fa-plus"></i></button>
+      <h2>Pages</h2><button id="addPageBtn"><i class="fa fa-plus"></i></button>
     </div>
     <ul id="pagesList">
-      ${pages
-        .map((p, i) => `
-      <li data-index="${i}">
-        <span class="pageTitle">${p.title}</span>
-        <div class="actions">
-          <button class="renamePage"><i class="fa fa-pen"></i></button>
-          <button class="deletePage"><i class="fa fa-trash"></i></button>
-        </div>
-      </li>`)
-        .join("")}
-    </ul>
-  `;
+      ${pages.map((p,i)=>`
+        <li data-index="${i}">
+          <span class="pageTitle">${p.title}</span>
+          <div class="actions">
+            <button class="renamePage"><i class="fa fa-pen"></i></button>
+            <button class="deletePage"><i class="fa fa-trash"></i></button>
+          </div>
+        </li>`).join("")}
+    </ul>`;
   attachListeners();
 }
 
@@ -36,41 +32,37 @@ function attachListeners() {
     const title = prompt("New page title:");
     if (title) addPage(title);
   };
-
-  document.querySelectorAll(".renamePage").forEach((btn) => {
-    btn.onclick = (e) => {
-      const li = e.target.closest("li");
-      const i = +li.dataset.index;
+  document.querySelectorAll(".renamePage").forEach(btn=>{
+    btn.onclick = e => {
+      const i = +e.target.closest("li").dataset.index;
       const t = prompt("Rename page:", pages[i].title);
       if (t) {
         pages[i].title = t;
-        pages[i].slug = t.toLowerCase().replace(/\s+/g, "-");
+        pages[i].slug = t.toLowerCase().replace(/\s+/g,"-");
         save();
       }
     };
   });
-
-  document.querySelectorAll(".deletePage").forEach((btn) => {
-    btn.onclick = (e) => {
+  document.querySelectorAll(".deletePage").forEach(btn=>{
+    btn.onclick = e => {
       const i = +e.target.closest("li").dataset.index;
       if (confirm("Delete this page?")) {
-        pages.splice(i, 1);
+        pages.splice(i,1);
         save();
       }
     };
   });
-
-  document.querySelectorAll(".pageTitle").forEach((span) => {
-    span.onclick = (e) => {
+  document.querySelectorAll(".pageTitle").forEach(span=>{
+    span.onclick = e => {
       const p = pages[+e.target.closest("li").dataset.index];
-      window.dispatchEvent(new CustomEvent("pageSelected", { detail: p }));
+      window.dispatchEvent(new CustomEvent("pageSelected",{detail:p}));
     };
   });
 }
 
 function addPage(title) {
-  const slug = title.toLowerCase().replace(/\s+/g, "-");
-  pages.push({ title, slug, theme: "light", hero: { transparentMenu: false }, blocks: [] });
+  const slug = title.toLowerCase().replace(/\s+/g,"-");
+  pages.push({ title, slug, theme:"light", hero:{ transparentMenu:false }, blocks:[] });
   save();
 }
 
@@ -79,7 +71,8 @@ function save() {
   renderPages();
 }
 
-window.addEventListener("load", () => setPages(getPages()));
+window.addEventListener("load",()=>setPages(getPages()));
+
 
 
 
