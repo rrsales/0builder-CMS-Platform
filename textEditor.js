841f1344b8bcfe8be05000beb1f75f4716f5057a
rrsales <josephbskinner1@gmail.com>
2025-12-04 20:05:06 -0500

    Delete textEditor.js

--- textEditor.js
+++
@@ -1,9 +0,0 @@
-// modules/textEditor.js
-export function initTextEditor() {
-  const panel = document.getElementById('textEditorPanel');
-  panel.innerHTML = `
-    <b>Text Editor</b><br>
-    <textarea style="width:100%;height:100px;">Edit page HTML here...</textarea>
-    <br><button onclick="alert('Save!')">Save</button>
-  `;
-}
